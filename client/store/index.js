bdcf711e909253ab341ff8c6e3dc13767698d71e
Dongwoo Kang <schach0415@yahoo.com>
2018-03-08 16:21:01 -0500

    contents store done

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import contents from './contents'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, contents})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
