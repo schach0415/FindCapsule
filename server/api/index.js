443d4ca80d39da14ecd5b6ebac719482729ac43b
Dongwoo Kang <schach0415@yahoo.com>
2018-03-08 11:31:06 -0500

    radorsRoute done

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/capsules', require('./capsules'))
+router.use('/radors', require('./radors'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
